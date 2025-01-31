972b1b57d7b58a95b8f2d7d4360bcf4b2fc63e4a
Ravimo <ravimo.raviolimavioli@gmail.com>
2025-01-31 13:23:13 +0700

    Stop.sh fix

--- script/stop.sh
+++ script/stop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo -e "Stopping webui docker...\n" &&
-sudo docker stop open-webui &&
-echo -e "Removing webui docker...\n" &&
-sudo docker container rm open-webui &&
+echo -e "Stopping webui docker...\n";
+sudo docker stop open-webui;
+echo -e "Removing webui docker...\n";
+sudo docker container rm open-webui;
 
-echo -e "Stopping Ollama...\n" &&
-sudo killall ollama &&
-sudo systemctl stop ollama.service &&
-echo "Done."
+echo -e "Stopping Ollama...\n";
+sudo killall ollama;
+sudo systemctl stop ollama.service;
+echo "Done.";
